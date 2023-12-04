2075b26f5ecbd7c36145f202fb88bd9235d31bd4
PavolBednarik <152331129+PavolBednarik@users.noreply.github.com>
2023-12-04 22:16:15 +0000

    Hello Word

--- src/taxcalculatorrefactor/TaxCalculatorRefactor.java
+++ src/taxcalculatorrefactor/TaxCalculatorRefactor.java
@@ -14,7 +14,7 @@ public class TaxCalculatorRefactor {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        System.out.println("Hello Word");
     }
     
 }
